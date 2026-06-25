8fa42562273d0c3663c431b8b4391c87d73352bb
Srajan M <4al23ai055@aiet.org.in>
2026-06-25 16:53:02 +0530

    git commands

---
+++ test.py
@@ -0,0 +1,24 @@
+# World","World",sep="#")
+# World",end="")
+# World")
+#
+#x="10.5"
+#print(int(float(x)))
+#print("\test\ntesting")
+#print(r"\test\ntesting")
+#print("\\test\\ntesting")
+#print(""" hello
+#      my name is
+#      srajan
+#      """)
+
+x="hello"
+#print(x[4])
+#print(x[0:3:2])
+#print(x[::-1])
+l=[1,2,3,4,5]
+print(l[::2])
+for i in range(1,10,2):
+    print(i)
+while True:
+    print("hello")
